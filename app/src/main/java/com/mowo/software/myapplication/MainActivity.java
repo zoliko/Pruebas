e36d5d73551c0d6cad765b4148b4b8724ddbb75d
zoliko <waxelo77@gmail.com>
2019-09-24 16:10:32 -0500

    paso de pantalla

---
+++ app/src/main/java/com/mowo/software/myapplication/MainActivity.java
@@ -0,0 +1,71 @@
+package com.mowo.software.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et1;
+    TextView etiqueta_menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_nip);
+        etiqueta_menu = (TextView) findViewById(R.id.etiqueta_menu);
+    }
+
+    //Metodo de igreso via NIP
+    public void NIP(View v)
+    {
+        String nip_String = et1.getText().toString();
+        Toast.makeText(this, "Usted ah ingresado con el NIP " + nip_String, Toast.LENGTH_SHORT).show();
+    }
+
+    //Metodos via scrollView
+    public void Seleccion(View view)
+        {
+            switch (view.getId())
+                {
+                    case R.id.codigoqr:
+                        Toast.makeText(this, "Escanee el codigo QR", Toast.LENGTH_SHORT).show();
+                        etiqueta_menu.setText("codigo QR");
+
+                        Intent nueva_pantalla = new Intent(this,pruebatransicion.class);
+                        startActivity(nueva_pantalla);
+                        //finish();
+
+
+                        break;
+                    case R.id.buscar:
+                        Toast.makeText(this, "FindYourself", Toast.LENGTH_SHORT).show();
+                        etiqueta_menu.setText("Buscar");
+                        break;
+                    case R.id.ubicacion:
+                        Toast.makeText(this, "Esta es su ubicacion actual", Toast.LENGTH_SHORT).show();
+                        etiqueta_menu.setText("Mapa");
+                        break;
+                    case R.id.emergencia:
+                        Toast.makeText(this, "Usted a solicitado una emergencia", Toast.LENGTH_SHORT).show();
+                        etiqueta_menu.setText("emergencia");
+                        break;
+                    case R.id.about:
+                        Toast.makeText(this, "Acerca de nosotros", Toast.LENGTH_SHORT).show();
+                        etiqueta_menu.setText("Acerca de nosotros");
+                        break;
+                    case R.id.contacto:
+                        Toast.makeText(this, "Contactanos!!!", Toast.LENGTH_SHORT).show();
+                        etiqueta_menu.setText("Contactanos");
+                        break;
+
+                }
+        }
+}
